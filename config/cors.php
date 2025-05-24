5a67c936876f50c640171657690a9a84bb67edef
marckstar-RjJ <202001810@est.umss.edu>
2025-05-24 00:39:04 -0400

    back con deploy

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,9 @@
 <?php
+
 return [
-    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'registro'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'https://tis-25-frontend.netlify.app')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
