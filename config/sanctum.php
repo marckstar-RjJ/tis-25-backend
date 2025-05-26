27ff75452519b2959c720633096bb3752ed0b5c0
marckstar-RjJ <202001810@est.umss.edu>
2025-05-26 11:01:30 -0300

    Actualizada configuracion de sesion y Sanctum

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:5173,127.0.0.1,127.0.0.1:5173,localhost:5176,127.0.0.1:5176,127.0.0.1:8002,localhost:8002,::1')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:5173,127.0.0.1,127.0.0.1:5173,localhost:5176,127.0.0.1:5176,127.0.0.1:8002,localhost:8002,::1,olimpiadas-sansi.netlify.app')),
     'guard' => ['web'],
     'expiration' => null,
     'middleware' => [
